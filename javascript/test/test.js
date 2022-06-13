5e42f11febf9849f192d6c808e4c295d48ad5bed
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-13 15:19:54 +0200

    updated

--- javascript/test/test.js
+++ javascript/test/test.js
@@ -1,40 +1,38 @@
+//fizzBuzz
+var output = [];
+var number = 1;
 
- var output=[];
- var number=1;
-
-function fizzBuzz(){
-   
-
-if(number%3===0&&number%5===0){
+function fizzBuzz() {
+  if (number % 3 === 0 && number % 5 === 0) {
     output.push("fizzbuzz");
-}else if(number%3===0){
+  } else if (number % 3 === 0) {
     output.push("fizz");
-}else if(number%5===0){
-    output.push("buzz")
-}else{
+  } else if (number % 5 === 0) {
+    output.push("buzz");
+  } else {
     output.push(number);
+  }
+  number++;
+  console.log(output);
 }
-number++;
-console.log(output)
 
+
+
+//leap year
+
+function leapYear(year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "Leap year";
+      } else {
+        return "Not a leap year";
+      }
+    } else {
+      return "Leap year";
+    }
+  } else {
+    return "Not a leap year";
+  }
 }
-
-// function fizzBuzz(){
-   
-
-//     if(number%3===0&&number%5===0){
-//        result= output.push("fizzbuzz");
-//     }
-   
-//     else{
-//        result= output.push(number);
-
-//     }
-//     number++;
-//     console.log(output)
-//     }
-    
-
-
-   
-  
+console.log(leapYear(2022))
