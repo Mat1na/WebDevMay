a8d8d2ed67f7f702eeaa228f18052eb30ca285ae
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-13 15:52:18 +0200

    updated

--- javascript/test/test.js
+++ javascript/test/test.js
@@ -15,24 +15,57 @@ function fizzBuzz() {
   number++;
   console.log(output);
 }
+// fizzBuzz();
 
 
 
 //leap year
 
-function leapYear(year) {
-  if (year % 4 === 0) {
-    if (year % 100 === 0) {
-      if (year % 400 === 0) {
-        return "Leap year";
-      } else {
-        return "Not a leap year";
-      }
-    } else {
-      return "Leap year";
-    }
-  } else {
-    return "Not a leap year";
-  }
+// function leapYear(year) {
+//   if (year % 4 === 0) {
+//     if (year % 100 === 0) {
+//       if (year % 400 === 0) {
+//         return "Leap year";
+//       } else {
+//         return "Not a leap year";
+//       }
+//     } else {
+//       return "Leap year";
+//     }
+//   } else {
+//     return "Not a leap year";
+//   }
+// }
+// console.log(leapYear(2022))
+
+
+
+
+//Guest list
+
+// var guestList=["Anna","Maria","John","Mike"];
+
+// var guestName=prompt("enter yor name");
+  
+//   if(guestList.includes(guestName)){
+//     alert("welcome")
+//   }else{
+//     alert("You are not invited")
+//   }
+
+
+
+
+//Who´s buying
+
+var people=["Anna","Maria","John","Mike"];
+
+
+function whoIsBuying(people){
+
+  var random=Math.floor(Math.random()*people.length)
+  return people[random]+" is gonna pay tody";
+ 
 }
-console.log(leapYear(2022))
+
+
