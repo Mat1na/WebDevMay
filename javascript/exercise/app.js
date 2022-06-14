0d35c9bc85d4ac16c9cf6eaa514f4ebf2dcea6d3
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-14 10:58:26 +0200

    updated

---
+++ javascript/exercise/app.js
@@ -0,0 +1,55 @@
+
+//Number method example
+
+// number method example
+
+// const Result = document.getElementById("result");
+// var userInput = prompt("Enter a number"); // return string
+// console.log(typeof userInput)
+// Result.innerText=userInput;
+
+// if(isNaN(userInput)){
+//     Result.innerText="Is not a number";
+// }else{
+//     Result.innerText=`This is ${userInput}`;
+
+// }
+
+
+
+var userInput=prompt("enter a number");
+var userInput2=prompt("enter a number");
+var results= document.getElementById("result");
+var number;
+var number2;
+var sum;
+
+
+if(isNaN(userInput)||isNaN(userInput2)){
+    alert("Is not a number");
+    userInput=prompt("enter a number again");
+    userInput2=prompt("enter a number again");
+    number=parseInt(userInput) ;
+    number2=parseInt(userInput2) ;
+    sum=number+number2;
+    console.log(sum);
+    results.innerText=`Your sum ${sum}`;
+
+}else{
+    number=parseInt(userInput) ;
+    number2=parseInt(userInput2);
+    sum=number+number2;
+    console.log(sum);
+    result.innerText=`your sum is ${sum}`;
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/numberMethods/number.js
@@ -0,0 +1,63 @@
+//Number Methods
+
+var result;
+var n1="10";
+var n2=10;
+var n3="10.5";
+var n4="10 15 20";
+var n5="10,15,20";
+var n6="9e+2";
+var n7=9e+2;
+const PI=3.14159265359;
+var n8=2000.54;
+var n9=2000.44;
+
+//toString() methods
+result=n2.toString();
+
+//parseInt()
+result=parseInt(n1);//returns interer representation of number
+result="10"+"10";
+result=parseInt("10")+parseInt("10");
+
+//parseFloat
+result=parseFloat(n3);//return floating point representation of number
+
+//toExponetial() method
+
+result=PI.toExponential()// return exponential representation of the number
+
+
+//toFixed()
+result=n8.toFixed();
+result=n9.toFixed();
+
+result=Number("10");
+result=Number(undefined);
+result=Number(null);
+result=Number(true);//=1
+result=Number(false);//=0
+
+result=Number.MAX_VALUE;
+result=Number.MIN_VALUE;
+result=Number.MAX_SAFE_INTEGER;
+result=Number.POSITIVE_INFINITY;
+result=Number.NEGATIVE_INFINITY;
+result=Number.NaN;
+result=Number.isFinite(10);
+result=Number.isFinite(Infinity);
+result=Number.isInteger("10");
+result=Number.isInteger(10);
+result=Number.isNaN(10/NaN);
+result=Number.isNaN("10"/"10");
+result=Number.isSafeInteger(10);
+
+var userinput="fgjhgjgj"
+// userinput=10
+if(Number.isNaN(userinput)){
+    console.log("Invalid")
+}else{
+    console.log("Valid")
+}
+
+console.log(result, typeof result);
