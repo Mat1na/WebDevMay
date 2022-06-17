27729f89aecc5dbd27344688cdbcadfca37d6fae
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-17 15:06:24 +0200

    updated

---
+++ javascript/assigment2/divisible.js
@@ -0,0 +1,22 @@
+const numbers = [12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200]
+var output=[]
+
+numbers.forEach(modulus)
+
+
+
+function modulus(item){
+
+    item%5===0
+   if(item<=150){
+    
+   output.push(item)
+   console.log(output)
+   } 
+  
+}
+
+
+
+
+
