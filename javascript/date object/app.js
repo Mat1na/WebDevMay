8fdf41cfa3fb0210ba3af487dc45e248d48c721a
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-20 18:11:18 +0200

    updated

--- javascript/date object/app.js
+++ javascript/date object/app.js
@@ -1,40 +1,41 @@
 let result;
-let lead=document.querySelector(".lead");
-
-let date=new Date();
-
+let lead = document.querySelector('.lead');
+let date = new Date();
+// let pastDate=new Date(1545,11,2,10,30,15)
+// console.log(date)
+// console.log(pastDate)
 
 // get full year
+result = date.getFullYear();
+lead.innerHTML = result
 
-result=date.getFullYear();
-lead.innerHTML=result;
+// get month date day
+lead.innerHTML = date.getMonth();
 
+lead.innerHTML = date.getDate();
+lead.innerHTML = date.getDay();
 
-// get month
-lead.innerHTML=date.getMonth();
-
-
-// tollocaleString
-result=date.toLocaleString()
-lead.innerHTML=date.toLocaleString("nl-BE")
-
-
-// window.setInterval(UpdateTime,1000)
-// function UpdateTime(){
-// var hours, minutes, seconds;
-// hours=date.getHours();
-// minutes=date.getMinutes();
-// seconds=date.getSeconds();
+// // toLocaleString
+// result = date.toLocaleString();
+// lead.innerHTML = date.toLocaleString();
+// lead.innerHTML = date.toLocaleString('nl-BE', { timeZone: 'Europe/Brussels', day: '2-digit', month: 'long', year: 'numeric' });
 
 
-//     lead.innerHTML=`${hours}:${minutes}:${seconds}`
+// window.setInterval(UpdateTime, 1000)
+
+// function UpdateTime() {
+//     let date2 = new Date()
+//     var hours, minutes, seconds;
+//     hours = date2.getHours();
+//     minutes = date2.getMinutes();
+//     seconds = date2.getSeconds();
+//     lead.innerHTML = `${hours}:${minutes}:${seconds}`
 // }
 
+// var DaysInNederlands= ['Zondag','Maandag','Dinsdag','Woensdag','Donderdag','Vrijdag','Zaterdag'];
 
-var DaysInNederlands=["zondag","Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag"]
-lead.innerHTML=DaysInNederlands[date.getDay()]
+// lead.innerHTML = DaysInNederlands[date.getDay()];
 
-result=date.setFullYear(2021,01,01)
-
-console.log(result)
-
+// result = date.setFullYear(2022,05,20)
+// lead.innerHTML =result
+// console.log(result)
