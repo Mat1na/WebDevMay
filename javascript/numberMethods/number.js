cc29a589fd5956c45ee2d1c13d5d2b2e813857a7
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-20 18:19:43 +0200

    updated

--- javascript/numberMethods/number.js
+++ javascript/numberMethods/number.js
@@ -28,36 +28,36 @@ result=parseFloat(n3);//return floating point representation of number
 result=PI.toExponential()// return exponential representation of the number
 
 
-//toFixed()
-result=n8.toFixed();
-result=n9.toFixed();
+// //toFixed()
+// result=n8.toFixed();
+// result=n9.toFixed();
 
-result=Number("10");
-result=Number(undefined);
-result=Number(null);
-result=Number(true);//=1
-result=Number(false);//=0
+// result=Number("10");
+// result=Number(undefined);
+// result=Number(null);
+// result=Number(true);//=1
+// result=Number(false);//=0
 
-result=Number.MAX_VALUE;
-result=Number.MIN_VALUE;
-result=Number.MAX_SAFE_INTEGER;
-result=Number.POSITIVE_INFINITY;
-result=Number.NEGATIVE_INFINITY;
-result=Number.NaN;
-result=Number.isFinite(10);
-result=Number.isFinite(Infinity);
-result=Number.isInteger("10");
-result=Number.isInteger(10);
-result=Number.isNaN(10/NaN);
-result=Number.isNaN("10"/"10");
-result=Number.isSafeInteger(10);
+// result=Number.MAX_VALUE;
+// result=Number.MIN_VALUE;
+// result=Number.MAX_SAFE_INTEGER;
+// result=Number.POSITIVE_INFINITY;
+// result=Number.NEGATIVE_INFINITY;
+// result=Number.NaN;
+// result=Number.isFinite(10);
+// result=Number.isFinite(Infinity);
+// result=Number.isInteger("10");
+// result=Number.isInteger(10);
+// result=Number.isNaN(10/NaN);
+// result=Number.isNaN("10"/"10");
+// result=Number.isSafeInteger(10);
 
-var userinput="fgjhgjgj"
-// userinput=10
-if(Number.isNaN(userinput)){
-    console.log("Invalid")
-}else{
-    console.log("Valid")
-}
+// var userinput="fgjhgjgj"
+// // userinput=10
+// if(Number.isNaN(userinput)){
+//     console.log("Invalid")
+// }else{
+//     console.log("Valid")
+// }
 
 console.log(result, typeof result);
