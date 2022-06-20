821031ca494372bc9a8deae74850cb07c919c9ea
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-06-20 11:09:10 +0200

    updated

--- javascript/dice game/dice.js
+++ javascript/dice game/dice.js
@@ -2,18 +2,18 @@ let score=document.querySelector(".score");
 let btn=document.querySelector(".btn");
 let results=document.querySelector(".results")
 
-
+let scores=0
 
 
 
 
 function rundomDiceNumbers(){
 
-var diceRoll1=Math.round((Math.random()*6)+1);
+var diceRoll1=Math.round((Math.random()*5))+1;
 
 console.log(diceRoll1);
 
-var diceRoll2=Math.round((Math.random()*6)+1);
+var diceRoll2=Math.round((Math.random()*5))+1;
 console.log(diceRoll2);
 
 results.innerHTML=`${diceRoll1} and ${diceRoll2}`
@@ -22,21 +22,13 @@ console.log(results);
 
 
 if(diceRoll1==diceRoll2){
-    
-    score="score:"+0;
-   
-    score++
+    scores++
+score.innerHTML=`your score is ${scores}`
 }
 else{
     results.innerHTML="Roll Again" 
 }
 }
-
-
-
-
-
-
 
 
 btn.addEventListener("click",rundomDiceNumbers)
