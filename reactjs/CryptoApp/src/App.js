530e5202903433a40da5cfdabaf172a3ee55a478
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-08-19 11:56:03 +0200

    updated

---
+++ reactjs/CryptoApp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PriceFinder from './PriceFinder'
+
+
+function App() {
+  return (
+    <Container className="bg-dark text-light  p-4" fluid={true}>
+        <PriceFinder/>
+      
+    </Container>
+  )
+}
+
+export default App
---
+++ reactjs/CryptoApp/src/PriceFinder.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { useState } from "react";
+import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
+
+export default function PriceFinder() {
+  return (
+    <div>PriceFinder</div>
+  )
+}
