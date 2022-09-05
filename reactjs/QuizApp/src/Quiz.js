3ab541423ac68cea89e7be442e807040cf4bbf17
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-05 15:45:45 +0200

    updated

--- reactjs/QuizApp/src/Quiz.js
+++ reactjs/QuizApp/src/Quiz.js
@@ -1,77 +1,95 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import { Form, Col, Button, Row } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 
 function Quiz() {
+  const [allData, setAllData] = useState([]);
 
-  const [questions, setQuestions]= useState ([])
+  const getQusetions = async () => {
+    let res = await fetch("https://opentdb.com/api.php?amount=10");
 
-  const getQusetions=async ()=>{
-  let res= await fetch("https://opentdb.com/api.php?amount=10&category=18")
- 
-  let data= await res.json()
-  setQuestions(data.results)
- 
-}
+    let data = await res.json();
+    setAllData(data.results);
+  };
 
-
-useEffect(()=>{
-  getQusetions ()
-  
-}, [])
-
-
+  useEffect(() => {
+    getQusetions();
+  }, []);
 
   return (
-    <>
-    questions.map((question, index)=>(
-    <div className='bg m-5 d-flex justify-content-center align-items-center'>
-    <Container className="d-flex justify-content-center align-items-center p-5 m-5 ">
-    <Row className=" form m-5 p-3">
+    <div className="bg m-5 d-flex justify-content-center align-items-center">
+      <Container className="d-flex justify-content-center align-items-center p-5 m-5 ">
+        <Row className=" form m-5 p-3">
+          {console.log(allData)}
 
-      <h1>{questions.question}</h1>
-      {console.log(questions)}
-      <fieldset>
-        <Form.Group as={Row} className="mb-3">
+          {
+          getNextQuestion=()=>{
+           
+          Object.keys(allData.length) !== 0 ? (
+            <>
+              {allData.map((question, index) => {
+                return (
+                  
+                  <>
+                  
+                    <p key={index}>{allData[index].question}</p>
+                    <Form>
+                      <div className="mb-3">
+                        <Form.Check
+                          type="radio"
+                          id="1"
+                          label={allData[index].correct_answer}
+                          value="correctanswer"
+                          name="question"
+                        />
+                        <Form.Check
+                          type="radio"
+                          id="2"
+                          label={allData[index].incorrect_answers[0]}
+                          value="incorrectanswer"
+                          name="question"
+                        />
+                        {allData[index].incorrect_answers.length > 2 ? (
+                          <Form.Check
+                            type="radio"
+                            id="3"
+                            label={allData[index].incorrect_answers[1]}
+                            value="incorrectanswer"
+                            name="question"
+                          />
+                        ) : (
+                          ""
+                        )}
+                        {allData[index].incorrect_answers.length > 2 ? (
+                          <Form.Check
+                            type="radio"
+                            id="4"
+                            label={allData[index].incorrect_answers[2]}
+                            value="incorrectanswer"
+                            name="question"
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </Form>
+                  </>
+                );
+              })}
+            </>
+          ) : (
+            ""
+          )}}
+
           <Col md={12}>
-            <Form.Check
-              type="radio"
-              label={questions.correct_answer}
-              name="formHorizontalRadios"
-              id="formHorizontalRadios1"
-            />
-            <Form.Check
-              type="radio"
-              label={questions.incorrect_answers[0]}
-              name="formHorizontalRadios"
-              id="formHorizontalRadios2"
-            />
-            <Form.Check
-              type="radio"
-              label={questions.incorrect_answers[1]}
-              name="formHorizontalRadios"
-              id="formHorizontalRadios3"
-            />
-             <Form.Check
-              type="radio"
-              label={questions.incorrect_answers[2]}
-              name="formHorizontalRadios"
-              id="formHorizontalRadios3"
-            />
+            <Button type="submit" className="w-100 mb-3" onClick={getNextQuestion()}>
+              Next
+            </Button>
           </Col>
-        </Form.Group>
-      </fieldset>
-
-      <Col md={12}>
-        <Button type="submit" className="w-100 mb-3">Submit answer</Button>
-      </Col>
-    </Row>
-  </Container>
-  </div>
-    
-    ))
-    </>
-  )
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
-export default Quiz
+export default Quiz;
