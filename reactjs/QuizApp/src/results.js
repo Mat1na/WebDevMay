16c039cb5b0c27082ba12f28bb0c423e12ebe4b4
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-11 15:46:12 +0200

    updated

--- reactjs/QuizApp/src/results.js
+++ reactjs/QuizApp/src/results.js
@@ -4,7 +4,7 @@ import { Card, Col, ListGroup, Row } from "react-bootstrap";
 
 
 function Results({ score, user, correct, allQuestions }) {
-  let index = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  let index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   return (
 
 
@@ -16,8 +16,8 @@ function Results({ score, user, correct, allQuestions }) {
           (<h2 className='text-center'>Hooray,Your score is {score}!</h2>) : (<h2 className='text-center '>Your score is {score}.
             Don´t give up <a href='/' className='text-decoration-none  '>try again.</a></h2>)}
       {console.log(allQuestions)}
-     
-  <Card as={Row}  >
+<br/>
+      {/* <Card as={Row}  >
       <Card.Header>Questions</Card.Header>
       <ListGroup as={Col} md={4} >
         {allQuestions.map((question)=>(<ListGroup.Item className='p-4' >{question}</ListGroup.Item>))}
@@ -27,8 +27,8 @@ function Results({ score, user, correct, allQuestions }) {
         <ListGroup as={Col} md={4}>
         {user.map((user)=>(<ListGroup.Item className='p-5'>{user}</ListGroup.Item>))}
              </ListGroup>
-          </Card>
-          {/* <Card as={Col} md={4} >
+          </Card> */}
+      {/* <Card as={Col} md={4} >
       <Card.Header>Correct Answer</Card.Header>
       <ListGroup variant="flush">
         {correct.map((cor)=>(<ListGroup.Item className='p-5'>{cor}</ListGroup.Item>))}
@@ -40,18 +40,25 @@ function Results({ score, user, correct, allQuestions }) {
         {user.map((user)=>(<ListGroup.Item className='p-5'>{user}</ListGroup.Item>))}
              </ListGroup>
           </Card> */}
-  
-      {/* <Card>
-        {allQuestions.map((question) => (<><Card.Header>{question} </Card.Header></>))}
 
+      <Row>
+        <Card as={Col} md={4}>
+          <h5 className='text-center'>Questions</h5>
+          {allQuestions.map((question, index) => (<><Card.Header className='m-0 p-0 d-flex justify-content-center align-items-center text-center result'><strong>{index}/</strong>{question} </Card.Header></>))}
+        </Card>
+        <Card as={Col} md={4}>
+          <h5 className='text-center'>Correct Answers</h5>
+          {correct.map((cor, index) => (<><Card.Body className=' justify-content-center align-items-center text-center'> <Card.Text ><strong>{index}/</strong> {cor} </Card.Text></Card.Body>
+          </>))}</Card>
+        <Card as={Col} md={4}>
+          <h5 className='text-center'>Your Answers</h5>
 
-        {correct.map((cor) => (<> <Card.Body><Card.Title> {cor} </Card.Title></Card.Body>
-        </>))}
-        {user.map((user) => (<> <Card.Body><Card.Text>
-          {user}
-        </Card.Text></Card.Body>
-        </>))}
-      </Card> */}
+          {user.map((user, index) => (<> <Card.Body className=' justify-content-center align-items-center text-center'><Card.Text>
+          <strong>{index}/</strong> {user}
+          </Card.Text></Card.Body>
+          </>))}</Card>
+      </Row>
+
 
 
 
