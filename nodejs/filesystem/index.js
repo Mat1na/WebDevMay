d214374bcd667fe9cb224e8b24717e0bd5069cf2
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-15 14:45:58 +0200

    updated

--- nodejs/filesystem/index.js
+++ nodejs/filesystem/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs'); // fs is a core module
 
 // read file
-fs.readFile('./test.txt','utf8',(err,data)=>{
+fs.readFile('./test.txt', 'utf8', (err, data) => {
     // console.log(typeof data) // convert buffer to string
     // console.log(data.split(",")[0]) // convert text into array
     // data.split(",").forEach(name=>console.log(name))
@@ -37,12 +37,12 @@ fs.readFile('./test.txt','utf8',(err,data)=>{
 //     })
 // })
 
-function ReadFiles(ext){
-    fs.readdir('./',(err,data)=>{
-        let TxtFiles = data.filter(file=>file.includes(ext)) // list of txt files
-        TxtFiles.forEach(txt=>{
-            fs.readFile(txt,'utf8',(err,data)=>{
-                console.log(data,txt)
+function ReadFiles(ext) {
+    fs.readdir('./', (err, data) => {
+        let TxtFiles = data.filter(file => file.includes(ext)) // list of txt files
+        TxtFiles.forEach(txt => {
+            fs.readFile(txt, 'utf8', (err, data) => {
+                console.log(data, txt)
             })
         })
     })
@@ -73,24 +73,89 @@ function ReadFiles(ext){
 // fs.readFile('users.json','utf8',(err,data)=>{
 //     let users = JSON.parse(data) // from string to array/object
 //     console.log(users,"before")
-//     users.push(5)
+//     users.push(5) // added new value
 //     console.log(users,"after")
 //     fs.writeFile('users.json',JSON.stringify(users),(err)=>{
 //         console.log('file is updated')
 //     })
 // })
 
-function AppendDataIntoFile(filename,NewValue){
-fs.readFile(filename,'utf8',(err,data)=>{
-    if(data===undefined){
-        fs.writeFile(filename,'[]',(err)=>console.log('file is created')) //if file does not exit
+// function isFileExist(filename) {
+//     return fs.existsSync(filename)
+// }
+
+// function createFile(filename) {
+//     fs.writeFile(filename, '[]', () => 'file is created')
+// }
+
+
+
+// async function AppendDataIntoFile(filename, NewValue) {
+//     let isFileFound = await isFileExist(filename)
+//     return {
+//         isFileFound,
+//         filename,
+//         NewValue
+//     }
+// }
+
+// AppendDataIntoFile('posts.json', '0')
+//     .then(data => {
+//         if(data.isFileFound === false){
+//             createFile(data.filename)
+//         }else{
+//             fs.readFile(data.filename,'utf8',(err,data)=>{
+//                 return data
+//             })
+//         }
+//     })
+//     .then(data=>{
+//         console.log(data)
+//     })
+
+// function AppendDataIntoJSONFile(fileName, newData) {
+//     fs.readFile(fileName, 'utf8', (err, data) => {
+//         if (err) { //als de file nog niet bestaat, wordt er nu eentje gemaakt
+//             fs.writeFile(fileName, '[]', (err) => console.log('file is created here'))
+//             setTimeout(function () {
+//                 AppendDataIntoJSONFile(fileName, newData)
+//             }, 3000)
+//         }
+
+//         else {
+//             let arr = JSON.parse(data) //from string to array
+//             arr.push(newData)
+//             fs.writeFile(fileName, JSON.stringify(arr), (err) => {
+//                 console.log('file is updated')
+//             })
+//         }
+//     })
+// }
+
+// AppendDataIntoJSONFile('posts.json',55)
+
+const http = require('http')
+
+
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/html'})
+    if(req.url === "/"){
+        // read html document
+        fs.readFile('index.html','utf8',(err,homepage)=>{
+            res.write(homepage)
+            res.end()
+        })
     }
-    // let arr=JSON.parse(data) //parse string into array
-    // arr.push(NewValue)//added new value in array
-    // //update file here
-    // fs.write(filename,JSON.stringify(arr), (err)=>{
-    //     console.log("file is appended")
-    // })
+    else if(req.url === '/login'){
+        fs.readFile('login.html','utf8',(err,login)=>{
+            res.write(login)
+            res.end()
+        })
+    }else{
+        res.write("not found")
+        res.end()
+    }
+    
 })
-}
-AppendDataIntoFile('post.json','0')
+
+server.listen(8080,()=>console.log('server is running...'))
