4cd666ce3e6685a9531fcad9a770893951f81a76
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-26 11:54:44 +0200

    updated

--- expessjs/httprequest/app.js
+++ expessjs/httprequest/app.js
@@ -1,39 +1,87 @@
 // first initialize npm init -y
 //then npm install express --save
 
-
 //import express module
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 //npm install dotenv --save   https://www.npmjs.com/package/dotenv
-require('dotenv').config()
-const bodyParser=require('body-parser');
-
+require("dotenv").config();
+const bodyParser = require("body-parser");
 
 //use bodyParser middleware
-app.use(bodyParser({
-    extended:true
-}))
+app.use(
+  bodyParser({
+    extended: true,
+  })
+);
+
+//midleware
+function isUserLogged(req, res, next) {
+  const { username } = req.query;
+
+  if (username === "admin") {
+    res.json({
+      username: username,
+
+      msg: "success",
+
+      //http://localhost:8080/?username=admin
+    });
+  } else {
+    res.json({
+      msg: "fail",
+
+      //http://localhost:8080/?username=asd
+    });
+  }
+
+  //next allow to continue
+
+  next();
+}
+
 //get request
-app.get('/',(req,res)=>{
-    res.json({id:1,title:'lorem ipsum', body:'lorem ipsum contennt'})
-})
-
-
+app.get("/", (req, res) => {
+  res.json({
+    id: 1,
+    title: "lorem ipsum",
+    body: "lorem ipsum contennt",
+  });
+});
 
 //npm install body-parser --save
 //post request
-app.post('/',(req,res)=>{
-    //mongo should save req body here
-    const {id,title,body}=req.body
-    res.json({
-       id,
-       title,
-       body
-    })
-})
+app.post("/",isUserLogged, (req, res) => {
+  //mongo should save req body here
+  const { id, title, body } = req.body;
+  res.json({
+    id,
+    title,
+    body,
+  });
+});
 
+//PUT request   A PUT request is used to send data to a server to create/update a resource.
+app.put("/", (req, res) => {
+  const { id } = req.query;
+  const { title, body } = req.body;
+  res.json({
+    title,
+    body,
+    message: `${id} is updated`,
+  });
+});
 
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on ${process.env.PORT}`)
-})
+//delete
+app.delete("/", (req, res) => {
+  const { id } = req.query;
+  const { _id } = req.body;
+
+  res.json({
+    message: `${id} is deleted | ${_id} is deleted`,
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on ${process.env.PORT}`);
+});
