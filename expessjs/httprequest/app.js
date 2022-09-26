57c9249173d6a26ea540ed2fc85fb2050b9d9303
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-26 11:09:34 +0200

    updated

--- expessjs/httprequest/app.js
+++ expessjs/httprequest/app.js
@@ -5,10 +5,35 @@
 //import express module
 const express = require('express');
 const app = express()
-require('dotenv').config()
-
 //npm install dotenv --save   https://www.npmjs.com/package/dotenv
+require('dotenv').config()
+const bodyParser=require('body-parser');
 
-server.listen(process.env.PORT,()=>{
+
+//use bodyParser middleware
+app.use(bodyParser({
+    extended:true
+}))
+//get request
+app.get('/',(req,res)=>{
+    res.json({id:1,title:'lorem ipsum', body:'lorem ipsum contennt'})
+})
+
+
+
+//npm install body-parser --save
+//post request
+app.post('/',(req,res)=>{
+    //mongo should save req body here
+    const {id,title,body}=req.body
+    res.json({
+       id,
+       title,
+       body
+    })
+})
+
+
+app.listen(process.env.PORT,()=>{
     console.log(`server is running on ${process.env.PORT}`)
 })
