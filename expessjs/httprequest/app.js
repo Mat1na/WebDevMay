d6088a0ca0a71df2150e076b3448194153045d52
5tamatia <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-26 10:54:49 +0200

    updated

--- expessjs/httprequest/app.js
+++ expessjs/httprequest/app.js
@@ -5,3 +5,10 @@
 //import express module
 const express = require('express');
 const app = express()
+require('dotenv').config()
+
+//npm install dotenv --save   https://www.npmjs.com/package/dotenv
+
+server.listen(process.env.PORT,()=>{
+    console.log(`server is running on ${process.env.PORT}`)
+})
