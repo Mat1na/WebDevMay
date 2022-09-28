af41c0063a029cc26a3bd5e29fdc43f3a5efad9d
Mat1na <Stamatia.Bampa@Student.intecbrussel.be>
2022-09-29 01:32:30 +0200

    updated

--- nodejs/expessjs/todoApp/browser/src/homepage.js
+++ nodejs/expessjs/todoApp/browser/src/homepage.js
@@ -1,60 +1,75 @@
 import React, { useEffect, useState } from "react";
 import { Button, Col, Form, InputGroup, Row } from "react-bootstrap";
-import { BsTrash} from 'react-icons/bs';
+import { BsTrash } from 'react-icons/bs';
 
-function Hompage({todos, setTodos}) {
-  const [todo, setTodo] = useState({});
+
+function Hompage({ todos, setTodos }) {
+  const [todo, setTodo] = useState(" ");
+  const [title, setTitle] = useState("")
+  const [completed, setCompleted] = useState(false)
 
   const fetchTodos = async () => {
     let res = await fetch(
-        'http://localhost:8080/todos'
+      'http://localhost:8080/todos'
     );
     let data = await res.json();
-    if(res.ok){
+    if (res.ok) {
 
-        setTodos(data.todos);
+      setTodos(data.todos);
     }
-    
+
   };
-  
+
 
 
   useEffect(() => {
     fetchTodos();
-     
+
   }, []);
-  
+
 
   const handleInput = (e) => {
-    console.log("Your input is", e.target.value);
-  
     setTodo(e.target.value);
+
   };
 
   const handleAddBtn = (e) => {
-    console.log("You clicked ", todo);
+    console.log("You added ", todo);
     e.preventDefault()
-    setTodo("")
-    setTodos([...todos, todo]);
-  };
+
+
+    const todo = { title, completed }
+    const res = async () => {
+      await fetch('http://localhost:8080/todos', {
+        method: 'POST',
+        body: JSON.stringify(todo),
+        headers: {
+          'content-type': 'aplication/json'
+        }
+      })
+      const data = await res.json()
+      setTodos([...todos, todo]);
+    }
+  }
 
   const handleDeleteBtn = () => {
     setTodos([])
+    console.log("after delete all", todos)
   };
   const doneBtn = (selectedTodo) => {
-   
-    const newTodos = todos.filter((todo) => todo!== selectedTodo);
+    console.log('deleted:', selectedTodo)
+    const newTodos = todos.filter((todo) => todo._id !== selectedTodo._id);
     // const newTodos=todos.splice(todos.indexOf(todo), 1);
     setTodos(newTodos);
-   
+    setCompleted(true)
   };
 
   return (
     <>
-    <Row className="p-5 m-5 justify-content-center align-items-center ">
-      <Col md={12}><h1 className="text-center p-3 m-3">Add your Todos</h1></Col>
-      <Col md={12}className="p-5 m-5 list text-align-center">
-             
+      <Row className="p-5 m-5 justify-content-center align-items-center ">
+        <Col md={12}><h1 className="text-center p-3 m-3">Add your Todos</h1></Col>
+        <Col md={12} className="p-5 m-5 list text-align-center">
+
           <InputGroup className="mb-3">
             <Form.Control
               onChange={handleInput}
@@ -62,6 +77,7 @@ function Hompage({todos, setTodos}) {
               aria-label="todo"
               aria-describedby="basic-addon1"
               value={todo}
+
             />
             <Button onClick={handleAddBtn}>Add ToDo</Button>
             {console.log(todos)}
@@ -72,24 +88,24 @@ function Hompage({todos, setTodos}) {
               <>
                 <div className="d-flex flex-row">
                   <div className="todo-container">
-                    <li key={todo._id}>{todo.title}</li>
-                    </div>
-                  <Button  onClick={() => doneBtn(todo)} className="deleteTodoBtn">
-                <BsTrash/>
-                </Button>
+                    <li key={todo._id} className="">{todo.title}</li>
                   </div>
-                <hr/>
+                  <Button onClick={() => doneBtn(todo)} className="deleteTodoBtn">
+                    <BsTrash />
+                  </Button>
+                </div>
+                <hr />
               </>
-              
+
             ))}
-             <Button onClick={ handleDeleteBtn} className="delete-all btn btn-danger">
-                 Clear List{console.log("after delete", todos)}
-                </Button>
+            <Button onClick={handleDeleteBtn} className="delete-all btn btn-danger">
+              Clear List
+            </Button>
           </ul>
-        
-      </Col>
+
+        </Col>
       </Row>
-      </>
+    </>
   );
 }
 
